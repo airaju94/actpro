7c9b88c5f775fd47dafcd9620241fb2d1de25d74
Aminul Islam <106031244+airaju94@users.noreply.github.com>
2025-04-16 19:24:57 +0600

    Delete app directory

--- app/view/layout.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    /* Prevent Direct Access */
-    defined( 'BASE' ) or die( 'No Direct Access!' );
-
-?>
-<?php echo View::component('include/header') ?>
-    <?php echo $content; ?>
-<?php echo View::component( 'include/footer' ); ?>
