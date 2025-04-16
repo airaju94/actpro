5e96db52f87dd30749c658979612fe2063bd8ef1
Aminul Islam <106031244+airaju94@users.noreply.github.com>
2025-04-16 19:25:35 +0600

    Delete config.php

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-    /* Security Constrant */
-    define( 'BASE', 'true' );
-
-    define('Root', __DIR__);
-    define('ROOT_DIR', strtolower( basename( Root ) ) ); // must use lowercase directory name
-
-    header( "Location: ".Url::baseUrl().'/act_install/' );
